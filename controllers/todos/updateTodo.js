dc00df3a7051357df8527154e07bcf65a5704cf9
Olena Voitsekhivska <mirkbog@gmail.com>
2022-09-02 01:49:37 +0300

    toggle

--- controllers/todos/updateTodo.js
+++ controllers/todos/updateTodo.js
@@ -6,9 +6,11 @@ const { requestError } = require("../../helpers");
 const updateTodo = async (req, res) => {
   const { id } = req.params;
   console.log(req.body)
-  const {complete:prevComplete} = await Todo.findById(id)
+  // const {complete:prevComplete} = await Todo.findById(id)
+  const prevTodo = await Todo.findById(id)
 
-  const result = await Todo.findByIdAndUpdate(id, { complete: !prevComplete });
+  // const result = await Todo.findByIdAndUpdate(id, { complete: !prevComplete });
+  const result = await Todo.findByIdAndUpdate(id, prevTodo);
 
   if (!result) {
     throw requestError(404, "Not found");
