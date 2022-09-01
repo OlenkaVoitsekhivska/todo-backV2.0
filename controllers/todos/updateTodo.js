52750506fadb34e80707df814efdcb539e5d993e
Olena Voitsekhivska <mirkbog@gmail.com>
2022-09-02 01:34:57 +0300

    all sorts of fixes

--- controllers/todos/updateTodo.js
+++ controllers/todos/updateTodo.js
@@ -5,8 +5,11 @@ const { requestError } = require("../../helpers");
 
 const updateTodo = async (req, res) => {
   const { id } = req.params;
+  console.log(req.body)
   const {complete:prevComplete} = await Todo.findById(id)
+
   const result = await Todo.findByIdAndUpdate(id, { complete: !prevComplete });
+
   if (!result) {
     throw requestError(404, "Not found");
   }
--- routes/api/todos.js
+++ routes/api/todos.js
@@ -19,17 +19,28 @@ const { wrapper } = require("../../helpers/wrapper");
 // router.post("/", authenticate, validateBody(postSchema), wrapper(addTodo));
 
 // router.delete("/:id", idValid, authenticate, wrapper(removeTodo));
+
+
 router.get("/",  wrapper(listTodos));
 
+// router.post("/", wrapper(addTodo));
+router.post("/", async(req,res,next)=>{
+  await addTodo();
+  return await listTodos();
+});
+
+// router.delete("/:id",  wrapper(removeTodo));
+router.delete("/:id",  async(req,res,next)=>{
+  await removeTodo();
+  return await listTodos();
+});
 
 
-router.post("/", wrapper(addTodo));
-
-router.delete("/:id",  wrapper(removeTodo));
-
-
-router.put("/:id", wrapper(updateTodo));
-
+// router.put("/:id", wrapper(updateTodo));
+router.put("/:id", async(req,res,next)=>{
+  await updateTodo();
+  return await listTodos();
+});
 
 
 module.exports = router;
