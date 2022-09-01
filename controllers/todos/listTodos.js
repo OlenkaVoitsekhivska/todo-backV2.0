cc0db2af760ed342bf76588e0a0ca67d666cd81e
Olena Voitsekhivska <mirkbog@gmail.com>
2022-09-02 00:15:05 +0300

    list tweaks

--- controllers/todos/listTodos.js
+++ controllers/todos/listTodos.js
@@ -3,8 +3,14 @@ const { Todo } = require("../../models/todo");
 const listTodos = async (req, res) => {
 
   const result = await Todo.find({}) 
-
-  res.json(result);
+const updatedRes = result.map(({_id,desciption,complete})=>{
+return{
+  id:_id,
+  desciption,
+  complete
+}
+})
+  res.json(updatedRes);
 };
 
 module.exports = listTodos;
