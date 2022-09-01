4bb7abb626d6505b36b4c5a1a4b0844f509f8aba
Olena Voitsekhivska <mirkbog@gmail.com>
2022-09-02 00:16:49 +0300

    typo

--- controllers/todos/listTodos.js
+++ controllers/todos/listTodos.js
@@ -3,10 +3,10 @@ const { Todo } = require("../../models/todo");
 const listTodos = async (req, res) => {
 
   const result = await Todo.find({}) 
-const updatedRes = result.map(({_id,desciption,complete})=>{
+const updatedRes = result.map(({_id,description,complete})=>{
 return{
   id:_id,
-  desciption,
+  description,
   complete
 }
 })
