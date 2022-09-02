14c6a2a0cf9c3ad857b468f724d87b8a5c1bdecb
Olena Voitsekhivska <mirkbog@gmail.com>
2022-09-02 13:55:00 +0300

    add enhanced

--- controllers/todos/addTodo.js
+++ controllers/todos/addTodo.js
@@ -5,7 +5,11 @@ const addTodo = async (req, res) => {
     // const result = await todo.create({...req.body, owner});
 
     const result = await Todo.create(req.body);
-    res.status(201).json(result);
+    // res.status(201).json(result);
+    res.status(201).json({
+      message:"Todo successfully added",
+      id:result._id
+    });
   };
 
 module.exports = addTodo;
