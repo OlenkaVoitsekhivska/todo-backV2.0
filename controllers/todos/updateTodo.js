585b3db9ad2c60149795cc3e75554e98c73331aa
Olena Voitsekhivska <mirkbog@gmail.com>
2022-09-02 14:33:19 +0300

    put for editTodo

--- controllers/todos/updateTodo.js
+++ controllers/todos/updateTodo.js
@@ -5,17 +5,22 @@ const { requestError } = require("../../helpers");
 
 const updateTodo = async (req, res) => {
   const { id } = req.params;
+  const {description} = req.body;
   console.log(req.body)
-  const {complete:prevComplete} = await Todo.findById(id)
+  // const {complete:prevComplete} = await Todo.findById(id)
 
 
-  const result = await Todo.findByIdAndUpdate(id, { complete: !prevComplete });
+  const result = await Todo.findByIdAndUpdate(id, {description});
 
 
   if (!result) {
     throw requestError(404, "Not found");
   }
-  res.json(result);
+  // res.json(result);
+  res.json({
+    message:"Todo updated successfully",
+    id:result._id
+  });
 };
 
 module.exports = updateTodo;
